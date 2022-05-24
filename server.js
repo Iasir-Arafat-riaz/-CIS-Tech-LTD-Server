a6bf2d014558ff13641cf5534c4b803d38ceb6f0
Iasir-Arafat-riaz <76946854+Iasir-Arafat-riaz@users.noreply.github.com>
2022-05-24 06:22:41 +0600

    customer detail add successfull

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-//user: CIS-Tech-LTD
-//pass: MW5WV2LMKotvjVE1
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gukqi.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
